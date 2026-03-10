f72d74a88ef4d02612688ae8c3ecd779495ce092
Ty Paragas <ty.paragas01@gmail.com>
2026-03-10 16:43:00 -0500

    Add user/pass in .env

--- Back-end/src/main/java/com/springNAS/Back_end/Controllers/LoginController.java
+++ Back-end/src/main/java/com/springNAS/Back_end/Controllers/LoginController.java
@@ -1,5 +1,6 @@
 package com.springNAS.Back_end.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+  @Value("${USERNAME}")
+  private String username;
+  
+  @Value("${PASS}")
+  private String pass;
   
   @PostMapping()
-  public boolean login()
+  public boolean login(String username, String password)
   {
-    return true;
+    return this.username.equals(username) && this.pass.equalsIgnoreCase(password);    
   }
 }
