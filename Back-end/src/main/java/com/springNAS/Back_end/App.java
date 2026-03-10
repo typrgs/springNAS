d3469f08e44ab8a8caf842c9769945dbef258810
Ty Paragas <ty.paragas01@gmail.com>
2026-03-10 16:04:43 -0500

    Rename backend entry point to App.java

--- Back-end/src/main/java/com/springNAS/Back_end/App.java
+++ Back-end/src/main/java/com/springNAS/Back_end/App.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndApplication {
+public class App {
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
